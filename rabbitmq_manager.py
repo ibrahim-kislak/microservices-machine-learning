7eea0ffa7a872f6818979f517d6275139e8bc471
kislak-ibrahim <ibothelax@gmail.com>
2026-07-13 21:33:09 +0300

    init

---
+++ rabbitmq_manager.py
@@ -0,0 +1,36 @@
+import pika
+
+class RabbitMQManager:
+    def __init__(self, host='localhost',):
+        self.host = host
+        self.connection = None
+        self.channel = None
+
+    def connect(self):
+        """Establish a connection to RabbitMQ and declare the queue."""
+        if self.connection is None or self.connection.is_closed:
+            self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.host))
+            self.channel = self.connection.channel()
+            self.channel.queue_declare(queue=self.queue)
+        
+    def get_channel(self):
+        if self.channel is None or self.channel.is_closed:
+            self.connect()
+        return self.channel
+
+    def publish_message(self, message: str, routing_key: str, exchange: str = ''):
+  
+        ch = self.get_channel()  
+        ch.basic_publish(
+            exchange=exchange,
+            routing_key=routing_key,  
+            body=message
+        )
+
+    def close_connection(self):
+        
+        if self.connection and  self.connection.is_open:
+            self.connection.close()
+            print("RabbitMQ connection closed.")
+
+rabbitmq_service=RabbitMQManager(host='localhost')
