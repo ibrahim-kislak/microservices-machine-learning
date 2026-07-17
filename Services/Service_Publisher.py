02f5d1269e43d4039e433ddf0f6c319fc44e4e7d
kislak-ibrahim <ibothelax@gmail.com>
2026-07-17 23:26:37 +0300

    3 patient and Redis added

--- Services/Service_Publisher.py
+++ Services/Service_Publisher.py
@@ -5,35 +5,56 @@ REDIS = redis.redis_service
 RABBIT = rmq.rabbitmq_service
 
 channel=RABBIT.get_channel()
-channel.exchange_declare(exchange='payment_exchange', exchange_type='fanout', durable=True)
+channel.exchange_declare(exchange='prediction_exchange', exchange_type='fanout', durable=True)
+channel.queue_declare(queue='prediction_queue', durable=True)
+channel.queue_bind(exchange='prediction_exchange', queue='prediction_queue')
 
+channel.queue_declare("logging_queue",durable=True)
+channel.queue_bind(exchange="prediction_exchange", queue="logging_queue")
+patients = [
+    {
+        "patient_id": "P_1001",
+        "age": 67.0,
+        "bmi": 36.6,
+        "hypertension": 1,
+        "smoking_status": "formerly smoked",
+        "work_type": "Private",
+        "gender": "Male",
+        "Residence_type": "Urban"
+    },
+    {
+        "patient_id": "P_1002",
+        "age": 45.0,
+        "bmi": 22.1,
+        "hypertension": 0,
+        "smoking_status": "never smoked",
+        "work_type": "Govt_job",
+        "gender": "Female",
+        "Residence_type": "Rural"
+    },
+    {
+        "patient_id": "P_1003",
+        "age": 78.0,
+        "bmi": 29.4,
+        "hypertension": 1,
+        "smoking_status": "smokes",
+        "work_type": "Self-employed",
+        "gender": "Male",
+        "Residence_type": "Urban"
+    }
+]
 
-payment_details = {
-    "amount": 100.0,
-    "payment_id": "pay_123456",
-    "currency": "USD",
-    "payment_method": "credit_card",
-    "card_number": "4111111111111111",
-    "is_paid": False}
-
-def Process_payment (payment_details):
-    payment_id = payment_details["payment_id"]
-    redis_key = f"payment_status:{payment_id}"
-    cached_status = REDIS.get_value(redis_key) 
+def process_patient (patient):
+    patient_id=patient["patient_id"]
+    redis_key=f"stroke_prediction :{patient_id}"
+    cached_status=REDIS.get_value(redis_key)
     
     if cached_status is not None:
-        print(f"[CACHE HIT] Payment with ID {payment_id} has already been processed. Cached status: {cached_status}")
-        payment_details["is_paid"] = True
-        RABBIT.publish_message(message=json.dumps(payment_details), exchange='payment_exchange', routing_key='')
-        return
-    else:
-        print(f"[CACHE MISS] Processing payment with ID {payment_id}.")
-        payment_details["is_paid"] = True
-        message = json.dumps(payment_details)
-        print("Payment processed successfully.")   
+        print(f"Cache Hit: {patient_id} status: {cached_status}")
+    else: 
+        print(f"Cache Miss: {patient_id} Sent to the machine to prediction...")
+        msg=json.dumps(patient)
+        RABBIT.publish_message(message=msg,exchange="prediction_exchange",routing_key="")
+        REDIS.set_value(redis_key,"işleme alindi veya tahmin edildi",ttl_sec=360)
         
-        RABBIT.publish_message(message=message, exchange='payment_exchange', routing_key='')
-        REDIS.set_value(redis_key, "paid", ttl_sec=360)  
-    
-    
-Process_payment(payment_details)
+        
