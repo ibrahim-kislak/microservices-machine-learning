5cfd985daf87afee1e3075d5bf25b48ae6487548
kislak-ibrahim <ibothelax@gmail.com>
2026-08-02 18:19:16 +0300

    Prometheus Manager added

--- Manager/Prometheus_Manager.py
+++ Manager/Prometheus_Manager.py
@@ -2,16 +2,19 @@ import os
 import time
 from typing import Optional,Generator
 from contextlib import contextmanager
-from prometheus_client import start_http_server,histogram,Counter,Gauge
+from prometheus_client import start_http_server,Histogram,Counter,Gauge
 
 class PrometheusManager:
     def __init__(self,host : Optional[str] = None , port : Optional[int] = None)->None:
-        self.host:str = host or os.getenv('PROMETHEUS_HOST','0.0.0.0'),
-        self.port:int = port or int(os.getenv('PROMETHEUS_PORT',8000))
-        self._is_server_started:bool = False
+        env_host = os.getenv('PROMETHEUS_HOST', '0.0.0.0')
+        env_port = os.getenv('PROMETHEUS_PORT', '8000')
+
+        self.host: str = str(host if host is not None else env_host)
+        self.port: int = int(port if port is not None else env_port)
+        self._is_server_started: bool = False
        
         self.prediction_request_counter:Counter = Counter('prediction_requests_total','Total number of prediction requests',labelnames=["status"])
-        self.prediction_duration_seconds:histogram = histogram('prediction_duration_seconds','Duration of prediction requests in seconds',buckets=(0.01, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0))
+        self.prediction_duration_seconds:Histogram = Histogram('prediction_duration_seconds','Duration of prediction requests in seconds',buckets=(0.01, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0))
         self.prediction_active:Gauge = Gauge('prediction_active_requests','Number of active prediction requests')
 
     def start_server(self)->None:
